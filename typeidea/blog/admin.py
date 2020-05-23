d31e448260becbd027d847732f4fac02ae4213e6
miniyang9 <958783491@qq.com>
2020-05-23 23:34:49 +0800

    '完成自定义list页面'

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -33,11 +33,26 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+    title = "分类过滤器"
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'status', 'created_time', 'operator')
+    list_display = ('title', 'category', 'status', 'created_time', 'operator', 'owner')
     list_display_links = []
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
@@ -53,9 +68,10 @@ class PostAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        print(f"request: {request}")
-        print(f"obj: {obj}")
-        print(f"form: {form}")
-        print(f"change: {change}")
         return super(PostAdmin, self).save_model(self, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)   # 重写父类方法
+        return qs.filter(owner=request.user)
+
 
