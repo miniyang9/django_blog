d6deff2308af5c728dc1fa142a95324b76025f7f
miniyang9 <958783491@qq.com>
2020-05-24 16:46:48 +0800

    基本页面开发

---
+++ typeidea/typeidea/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
